9fd44fe99f6f8ae37c27b08cd31bd91f8a38b3fe
jiangchao <jiangchaobj@yahoo.com>
2015-12-28 20:18:14 +0800

    Init

---
+++ UITableViewDemo/UITableViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITableViewDemo
+//
+//  Created by jiangchao on 15/12/28.
+//  Copyright © 2015年 jiangchao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
